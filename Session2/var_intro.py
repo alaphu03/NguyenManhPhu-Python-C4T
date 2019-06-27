23a24a541e966dd51640582cd2b64e67419fcc8c
alaphu03 <phunguyen2004@gmail.com>
2019-06-27 21:16:07 +0700

    enter

--- Session2/var_intro.py
+++ Session2/var_intro.py
@@ -1,3 +1,3 @@
-year_of_birth = input("year_of_birth:")
-age = 2019 - int(year_of_birth)
-print("now, your are", age, "years old")
+# year_of_birth = input("year_of_birth:")
+# age = 2019 - int(year_of_birth)
+# print("now, your are", age, "years old")
