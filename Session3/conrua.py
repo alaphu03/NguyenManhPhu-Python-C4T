3527c3d70863ad0036b752797dbc1b7657e5ec92
alaphu03 <phunguyen2004@gmail.com>
2019-07-02 21:28:10 +0700

    enter

--- Session3/conrua.py
+++ Session3/conrua.py
@@ -12,15 +12,40 @@
 #         forward(100)
 #         left(90)
 #     right(30)   
+# from turtle import *
+# shape("turtle")
+# color("cyan")
+# for i in range(4):
+#     forward(5)
+#     left(90)
+# for i in range(29):
+#     backward(5)
+#     right(90)
+#     for i in range(5):
+#         forward(5)
+#         left(90) 
+# from turtle import *
+# shape("turtle")
+# color("cyan")
+# fillcolor("yellow")
+# begin_fill()
+# for i in range(3):
+#     forward(300)
+#     left(120)
+# end_fill()
+# from turtle import *
+# shape("turtle")
+# color("cyan")
+# fillcolor("yellow")
+# begin_fill()
+# circle(80)
+# end_fill()
 from turtle import *
 shape("turtle")
 color("cyan")
-for i in range(4):
-    forward(5)
-    left(90)
-for i in range(29):
-    backward(5)
-    right(90)
-    for i in range(5):
-        forward(5)
-        left(90) 
+for i in range(6):
+    circle(80)
+    left(60)
+hideturtle()
+
+
---
+++ Session3/sum.py
@@ -0,0 +1,5 @@
+a = 0 
+n = int(input("so nguoi dung nhap:"))
+for i in range(n+1):
+    a = a + i
+print(a)
