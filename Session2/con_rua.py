0dafe29a84a3d093fd90af827307389dd0e24af4
alaphu03 <phunguyen2004@gmail.com>
2019-07-02 19:32:09 +0700

    enter

--- Session2/con_rua.py
+++ Session2/con_rua.py
@@ -1,16 +1,17 @@
-from turtle import *
-shape('turtle')
-color('cyan')
-speed(-1)
-for i in range(3):
-    forward(100)
-    left(120)
-for i in range(4):
-    forward(100)
-    left(90)
-for i in range(5):
-    forward(100)
-    left(72)
+# from turtle import *
+# shape('turtle')
+# color('cyan')
+# speed(-1)
+# for i in range(3):
+#     forward(100)
+#     left(120)
+# for i in range(4):
+#     forward(100)
+#     left(90)
+# for i in range(5):
+#     forward(100)
+#     left(72)
+
     
         
 
