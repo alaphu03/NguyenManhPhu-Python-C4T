db8bc2611f31ccba6a8a0bb934db1e8a255c8019
alaphu03 <phunguyen2004@gmail.com>
2019-07-02 19:48:57 +0700

    enter

--- Session3/conrua.py
+++ Session3/conrua.py
@@ -1,6 +1,14 @@
+# from turtle import *
+# shape("turtle")
+# color("cyan")
+# for i in range(4):
+#     forward(100)
+#     left(90)
 from turtle import *
 shape("turtle")
-color("cyan")
-for i in range(4):
-    forward(100)
-    left(90)
+color("cyan"
+ for i in range(3):
+    for i in range(4):
+        forward(100)
+        left(90)
+    right(30)   
