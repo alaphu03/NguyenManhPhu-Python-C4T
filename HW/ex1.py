cff798a28ed103bcab27d1f22ab28ef7dba12cd9
alaphu03 <phunguyen2004@gmail.com>
2019-07-04 08:58:16 +0700

    enter

---
+++ HW/ex1.py
@@ -0,0 +1,17 @@
+#        ex1) Tinh chi so BMI
+
+
+# height = int(input("height(cm):"))
+# weight = int(input("weight(kg):"))
+# h = height / 100
+# BMI = weight / (h * h)
+# if BMI < 16 :
+#     print("Now,your BMI is ", BMI, "you are severely underweight")
+# elif BMI < 18.5 :
+#     print("Now,your BMI is ", BMI, "you are underweigh")   
+# elif BMI < 25 :
+#     print("Now,your BMI is ", BMI, "you are normal")
+# elif BMI < 30 :
+#     print("Now,your BMI is ", BMI, "you are overweight")
+# else :
+#     print("Now,your BMI is ", BMI, "you are obese")
---
+++ HW/ex2.py
@@ -0,0 +1,7 @@
+# ex2) tinh luy thua
+
+# a = 1 
+# n = int(input("so nguoi dung nhap:"))
+# for i in range(1, n+1):
+#     a = a * i
+# print("luy thua so nguoi dung nhap:", a)
