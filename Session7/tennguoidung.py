0c60442f35af8aaf4c71faf93ff24a957823b7f7
alaphu03 <phunguyen2004@gmail.com>
2019-07-09 19:43:47 +0700

    enter

--- Session7/tennguoidung.py
+++ Session7/tennguoidung.py
@@ -2,6 +2,7 @@ while True:
     txt = input("Nhap ten cua ban :")
     if txt.isalpha():
         txt2 = input("Nhap ho cua ban:")
+        while True:
             if txt2.isalpha():
                 print("ho va ten cua ban la:", txt2, txt)
                 break
