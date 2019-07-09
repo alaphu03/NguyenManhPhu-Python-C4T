969ab24822418f86705409dee679bab2b952f121
alaphu03 <phunguyen2004@gmail.com>
2019-07-09 19:57:37 +0700

    enter

---
+++ Session7/chuyenchuhoa.py
@@ -0,0 +1,7 @@
+while True:
+    txt = input("Nhap chu cua ban :")
+    if txt.isalpha():
+        print("chu in hoa cua chu ban vua nhap la:", txt.upper())
+        break
+    else:
+        print("nhap chu")
