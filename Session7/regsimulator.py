969c7f8b9bc0010db9dbaa6935362eb72e83226f
alaphu03 <phunguyen2004@gmail.com>
2019-07-11 11:25:58 +0700

    enter

---
+++ Session7/regsimulator.py
@@ -0,0 +1,46 @@
+# n = input("Nhap tai khoan cua ban: ")
+# h = input("nhap email cua ban :")
+# j = input("nhap mat khau cua ban:")
+
+
+
+
+
+# n = input("Nhap tai khoan cua ban: ")
+# h = input("nhap email cua ban :")
+# j = input("nhap mat khau cua ban:")
+# while True:
+#     l = input("nhap lai mat khau cua ban:")
+#     if j == l:
+#         print("tai khoan cua ban da tao thanh cong")
+#         break
+#     else:
+#         print("mat khau khong chinh xac")
+
+
+
+# n = input("Nhap tai khoan cua ban: ")
+# while True:
+#     email = input("nhap email cua ban :")
+#     if ("gmail" in email or "yahoo" in email) and "@" in email and ".com" in email:
+#         break
+#     else:
+#         print("email cua ban khong hop le")
+#         continue
+# while True:
+#     j = input("nhap mat khau cua ban:")
+#     if j.isdigit() or len(j) <= 8:
+#         print("mat khau phai bao gom ca so va chu va co tren 8 ki tu")
+#         continue  
+#     else:
+#         break
+# while True:
+#     l = input("nhap lai mat khau cua ban:")
+#     if j == l:
+#         print("tai khoan cua ban da tao thanh cong")
+#         break
+#     else:
+#         print("mat khau khong chinh xac")
+
+
+
