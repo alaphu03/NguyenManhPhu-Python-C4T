d6df803a39d33255283c8441d7fab36103deaeb7
alaphu03 <phunguyen2004@gmail.com>
2019-07-11 13:34:05 +0700

    enter

--- HWSession7+8/ruaex.py
+++ HWSession7+8/ruaex.py
@@ -1,18 +1,26 @@
-# colors = ['red', 'blue', 'brown', 'yellow', 'grey']
-# from turtle import *
-# for i in range(5):
-#     color(colors[i])
-#     for k in range(5):
-#         for j in range(k):
-#             forward(100)
-#             left(360/j)
+# from turtle import*
+# colors=["red", "blue", "brown", "yellow", "grey"]
+# for i, k in enumerate(colors):
+#     color(k)
+#     for h in range(i+3):
+#         forward(100)
+#         left(360/ (i+3) )
 # mainloop()
+
+
 
 from turtle import*
 colors=["red", "blue", "brown", "yellow", "grey"]
-for i,k in enumerate(colors):
+for k in enumerate(colors):
     color(k)
-    for i in range(i+3):
-        forward(100)
-        left(360/(i+3))
+    fillcolor(k)
+    begin_fill()
+    for i in range(5):
+        if i%2 ==0:
+            forward(100)
+            right(90)
+        else:
+            forward(50)
+            right(90)
+    end_fill()
 mainloop()
