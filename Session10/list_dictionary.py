857410e57545d84a29fc4eef0222f1c64759143f
alaphu03 <phunguyen2004@gmail.com>
2019-07-11 22:06:23 +0700

    enter

--- Session10/list_dictionary.py
+++ Session10/list_dictionary.py
@@ -1,42 +1,17 @@
-# H ={
-#     "name": "Huy",
-#     "Role": "Waiter",
-#     "Hour": "12 hours",
-#     "salary per hour": 0,8
-# }
-# T ={
-#     "name": "Tung",
-#     "Role": "Cook",
-#     "Hour": "24 hours",
-#     "salary per hour": 1,5
-# }
-
-# D{
-#     "name": "Duc",
-#     "Role": "Manager",
-#     "Hour": "20 hours",
-#     "salary per hour": 2
-# }
-# pp = [H, D, T]
-# print(pp)
-
-
-nguoi = [ { "name" :  "Huy", "Role" :"Waiter" , "Hour": "12 hours", "salary per hour": 0,8} ,
-          { "name" :  "Tung", "Role" :"Cooker" , "Hour": "24 hours", "salary per hour": 1,5} ,
-          { "name" :  "Duc", "Role" :"Manager" , "Hour": "20 hours", "salary per hour": 2}
-        ]
+dict1 = {"name" :  "Huy", "Role" :"Waiter" , "Hour": "12 hours", "salary per hour": "0.8"}
+dict2 = {"name" :  "Tung", "Role" :"Cooker" , "Hour": "24 hours", "salary per hour": "1,5"}
+dict3 = {"name" :  "Duc", "Role" :"Manager" , "Hour": "20 hours", "salary per hour": "2"}
+nguoi = [dict1, 
+         dict2 ,
+         dict3]
+print(nguoi)
+dict1["salary"]= 9.6
+dict2["salary"]= 36
+dict3["salary"]= 40
+dict1["status"]= "single"
+dict2["status"]= "single"
+dict3["status"]= "single"
 print(nguoi)
 
 
-Blacksburg_Forecast = [ { 'humidity' :  20, 'temperature' : 78, 'wind' :  7} ,
-                        { 'humidity' :  50, 'temperature' : 61, 'wind' : 10} ,
-                        { 'humidity' : 100, 'temperature' : 81, 'wind' :  5} ,
-                        { 'humidity' :  90, 'temperature' : 62, 'wind' : 15} ,
-                        { 'humidity' :  30, 'temperature' : 84, 'wind' : 19} ,
-                        { 'humidity' :   0, 'temperature' : 66, 'wind' : 28} ,
-                        { 'humidity' :   0, 'temperature' : 87, 'wind' : 12} ,
-                        { 'humidity' :   0, 'temperature' : 68, 'wind' : 14} ,
-                        { 'humidity' :   0, 'temperature' : 86, 'wind' :  4} ,
-                        { 'humidity' :  60, 'temperature' : 68, 'wind' :  0}
-                       ]
 
