9319f5bc6eeebe4513ee398e4e9b1655a24c2001
alaphu03 <phunguyen2004@gmail.com>
2019-07-11 08:29:27 +0700

    enter

---
+++ Session8/CRUDsummary.py
@@ -0,0 +1,21 @@
+items =[]
+while True:
+    n = input("C:Creat/R:Read/U:Update/D:Delete/Q:Quit   Chon mot thao tac:")
+     if n == "C":
+        items.append(input("Nhap thu ban yeu thich: "))
+    elif n == "R":
+        if len(items) == 0:
+            print("Danh sach rong")
+        else:
+            print("Danh sach cua ban:")
+            for i in items:
+                print(i) 
+    elif n == "U":
+        location = int(input("Nhap vi tri thu ban muon thay doi: "))
+        items[location] = input("Thu ban muon thay doi: ")
+    elif n == "D":
+        location = int(input("Nhap vi tri thu ban muon xoa: "))
+        items.pop(location)
+    else:
+        break
+    print()
