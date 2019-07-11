3a5204093049047232ebc0be83f01360b31db35f
alaphu03 <phunguyen2004@gmail.com>
2019-07-11 13:51:03 +0700

    enter

--- HWSession7+8/ruaex.py
+++ HWSession7+8/ruaex.py
@@ -11,16 +11,18 @@
 
 from turtle import*
 colors=["red", "blue", "brown", "yellow", "grey"]
-for k in enumerate(colors):
+for k in colors:
     color(k)
     fillcolor(k)
     begin_fill()
-    for i in range(5):
-        if i%2 ==0:
-            forward(100)
-            right(90)
-        else:
-            forward(50)
-            right(90)
+    forward(100)
+    right(90)
+    forward(200)
+    right(90)
+    forward(100)
+    right(90)
+    forward(200)
+    right(90)
+    forward(100)       
     end_fill()
 mainloop()
